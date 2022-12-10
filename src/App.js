65aea4ca3320936716c1e3eed3f48921960b2751
Mayara Rocha <mayaraaraujorocha54@gmail.com>
2022-12-10 12:48:41 -0300

    aulafundamentos2-consicionais

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+const Title = () =>{
+  return(
+    <h1>
+    <span>olá </span>
+    <strong>mundo</strong>
+    !!!
+  </h1>
+  )
+}
 
+function MyButton(props){
+    return(
+    <button type="button">{props.label}</button>
+  )
+}
+function PessoaIdade(props){
+  return(
+    <strong>{props.label}</strong>
+  )
+}
+function Pessoa(props) {
+  return(
+    <section>
+      você é {props.idade >= 18 ? <PessoaIdade label='De Maior' /> : <PessoaIdade label='De menor' />}</section> //Operador ternário
+  )
+}
 function App() {
+const labelBotao = 'Entre aqui'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Title />
+   <Pessoa idade={19} />
+   <Pessoa idade={14} />
+   <Pessoa idade={30} />
+   <Pessoa idade={11} />
+   <div className="button1">
+      <MyButton label="Aperte Aqui"/>
+   </div>
+ 
+   <div className="button2">
+    <MyButton label={`${labelBotao}  !!`}/>
+   </div>
+   
     </div>
   );
 }
